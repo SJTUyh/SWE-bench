4291640381caf865f8a0d850dc5fd83dcca784e7
yanhe13 <yanhe13@huawei.com>
2025-11-11 16:02:57 +0800

    modify

--- swebench/harness/docker_build.py
+++ swebench/harness/docker_build.py
@@ -491,8 +491,9 @@ def build_container(
     if force_rebuild:
         remove_image(client, test_spec.instance_image_key, "quiet")
     if not test_spec.is_remote_image:
-        build_instance_image(test_spec, client, logger, nocache)
         print("111111111111111111111111111111111111111111111111111111111111")
+        build_instance_image(test_spec, client, logger, nocache)
+        #print("111111111111111111111111111111111111111111111111111111111111")
     else:
         try:
             client.images.get(test_spec.instance_image_key)
