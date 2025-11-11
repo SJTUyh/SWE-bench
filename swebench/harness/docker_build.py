b8d34e254541498fc64b4bba29d013fab839706b
yanhe13 <yanhe13@huawei.com>
2025-11-11 16:07:50 +0800

    modify

--- swebench/harness/docker_build.py
+++ swebench/harness/docker_build.py
@@ -496,8 +496,9 @@ def build_container(
         #print("111111111111111111111111111111111111111111111111111111111111")
     else:
         try:
-            client.images.get(test_spec.instance_image_key)
             print("111111111111111111111111111111111111111111111111111111111111")
+            client.images.get(test_spec.instance_image_key)
+            #print("111111111111111111111111111111111111111111111111111111111111")
         except docker.errors.ImageNotFound:
             try:
                 client.images.pull(test_spec.instance_image_key)
